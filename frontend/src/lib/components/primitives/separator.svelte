426399412950f6f3808fd97fa13ee64fb1505054
theerlant <ttrinanda2@gmail.com>
2025-12-01 17:36:47 +0800

    feat: login & register page + reactivity. TODO: Responsive UI

---
+++ frontend/src/lib/components/primitives/separator.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    type Direction = "Vertical" | "Horizontal"
+
+    interface Props {
+        direction: Direction;
+        className?: string;
+    }
+
+    let { direction, className }: Props = $props();
+</script>
+
+<div
+	class={`${direction === 'Horizontal' ? 'h-[1px] w-full' : 'w-[1px] h-full'} ${className}`}
+></div>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>IMPHNEN (Ingin Menjadi Pengusaha Handal Namun Enggan Ngebuku)</title>
+</svelte:head>
+
+{@render children()}
