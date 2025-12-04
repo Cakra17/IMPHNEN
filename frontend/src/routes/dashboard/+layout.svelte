08413117923a2bcb9c1719d695f99c7d865e3536
theerlant <ttrinanda2@gmail.com>
2025-12-04 15:34:25 +0800

    fix: added required page nav, changed feature to telegram bot

--- frontend/src/routes/dashboard/+layout.svelte
+++ frontend/src/routes/dashboard/+layout.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
-	import { Heading, P } from "flowbite-svelte";
-    import { ChevronDownIcon } from "@lucide/svelte"
+	import { Heading, Navbar, NavBrand, NavLi, NavUl, P } from "flowbite-svelte";
+    import { BellIcon, ChevronDownIcon } from "@lucide/svelte"
 	import Logo from "$lib/components/logo.svelte";
 	import Clock from "$lib/components/clock.svelte";
+	import Separator from "$lib/components/primitives/separator.svelte";
+
 	let { children } = $props();
+
+	import { page } from "$app/state";
 </script>
 
 <div class="font-jakarta w-screen h-screen bg-stone-50">
-	<div
-		class="sticky top-0 z-50 h-14 lg:h-18 grid grid-cols-3 bg-white w-full items-center px-4 md:px-12 lg:px-16 xl:px-24 border-b-1 border-teal-200 justify-between"
+	<Navbar
+		class="sticky top-0 z-50 h-14 lg:h-18 bg-white w-full items-center px-4 md:px-12 lg:px-16 xl:px-24 border-b-1 border-teal-200 flex flex-row items-center"
 	>
-		<a href="/dashboard" class="flex flex-row items-center gap-2">
+		<NavBrand href="/dashboard" class="flex flex-row items-center gap-2">
 			<Logo size="sm" />
 			<Heading tag="h6" class="hidden md:inline">IMPHNEN</Heading>
-		</a>
-		<div class="text-center">
-			<Heading tag="h6" class="md:hidden inline">IMPHNEN</Heading><span
-				class="font-semibold hidden md:inline"><Clock /></span
+		</NavBrand>
+		<NavUl>
+			<NavLi href="/dashboard" class={page.url.pathname === '/dashboard' ? 'underline' : ''}
+				>Dashboard</NavLi
 			>
-		</div>
-		<div class="flex flex-row gap-2 justify-end items-center">
+			<NavLi
+				href="/dashboard/orders"
+				class={page.url.pathname === '/dashboard/orders' ? 'underline' : ''}>Orderan</NavLi
+			>
+			<NavLi
+				href="/dashboard/analytics"
+				class={page.url.pathname === '/dashboard/analytics' ? 'underline' : ''}
+				>Analisa Keuangan</NavLi
+			>
+			<NavLi
+				href="/dashboard/config"
+				class={page.url.pathname === '/dashboard/config' ? 'underline' : ''}>Pengaturan</NavLi
+			>
+		</NavUl>
+		<div class="flex flex-row py-2 h-full gap-4 justify-end items-center">
 			<div class="hidden md:inline flex flex-col items-end">
 				<P weight="semibold" size="sm" align="right">John Doe</P>
 				<P size="xs" align="right">Toko Pak John</P>
@@ -29,7 +46,7 @@
 				<ChevronDownIcon />
 			</button>
 		</div>
-	</div>
+	</Navbar>
 	<div class="w-full px-4 md:px-12 lg:px-16 xl:px-24 py-8">
 		{@render children()}
 	</div>
